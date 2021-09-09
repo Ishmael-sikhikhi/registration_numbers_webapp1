eecbe0adf95f7f0b2037739af9320a51426ee90e
Ishmael-sikhikhi <i.sikhikhi@gmail.com>
2021-09-09 02:15:56 +0200

    add second db table

--- tables.sql
+++ tables.sql
@@ -1,4 +1,13 @@
+create table towns(
+town_id text not null primary key,
+town_name text not null
+);
+
 create table regNum (
 	id serial not null primary key,
-	reg_num text not null
+	reg_num text not null,
+	town_id char(2),
+	foreign key (towns_id) references towns(town_id)
 );
+
+alter table towns add constraint uniq_desc_constraint unique(town_name);
